c8bfd639ee2c4471e6ab7c2ef94020f7c52736ed
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-12-01 23:07:01 +0100

    day1 - part2

--- AdventOfCode2024/day1/main.cpp
+++ AdventOfCode2024/day1/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <sstream>
 #include <set>
+#include <unordered_map>
 using namespace std;
 
 std::vector<std::string> splitString(std::string& s, char delimiter, bool ignoreEmpty) {
@@ -25,8 +26,7 @@ std::vector<std::string> splitString(std::string& s, char delimiter, bool ignore
   return tokens;
 }
 
-
-int main() {
+void firstPartOfTheProblem() {
   ifstream inputFile("input.txt");
   string readLine;
   std::multiset<int> leftList;
@@ -52,6 +52,36 @@ int main() {
   }
 
   cout << "Sum of all distances: " << sumOfAllDistances << "\n";
+}
+
+int main() {
+
+  // firstPartOfTheProblem();
+
+  ifstream inputFile("input.txt");
+  string readLine;
+  std::unordered_map<int, int> leftList;
+  std::unordered_map<int, int> rightList;
+  int similarityScore;
+
+  if(inputFile.is_open()) {
+    while(getline(inputFile, readLine)) {
+      auto parts = splitString(readLine, ' ', true);
+      leftList[stoi(parts[0])]++;
+      rightList[stoi(parts[1])]++;
+    }
+    inputFile.close();
+  } else {
+    cerr << "Unable to open file" << endl;
+  }
+
+  for(const auto& pair : leftList) {
+    if(rightList.find(pair.first) != rightList.end()) {
+      similarityScore += pair.first * rightList.find(pair.first)->second;
+    }
+  }
+
+  cout << "Similarity score: " << similarityScore << endl; 
 
   return 1;
 }
