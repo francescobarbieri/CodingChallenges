8857d119daae44e9e5ff57855d4d4c206c56816d
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-12-02 22:57:55 +0100

    day2 - part2: it works

--- AdventOfCode2024/day2/mainPartTwo.cpp
+++ AdventOfCode2024/day2/mainPartTwo.cpp
@@ -30,9 +30,15 @@ std::vector<std::vector<int> > readReportsFromFile() {
 }
 
 bool isMonotonic(const std::vector<int>& vec) {
-  auto increasing = std::adjacent_find(vec.begin(), vec.end(), std::greater_equal<int>()) == vec.end();
-  auto decreasing = std::adjacent_find(vec.begin(), vec.end(), std::less_equal<int>()) == vec.end();
-
+  if (vec.size() <= 1) return true;
+  bool increasing = true;
+  bool decreasing = true;
+  
+  for (size_t i = 0; i < vec.size() - 1; ++i) {
+    if (vec[i] >= vec[i + 1]) increasing = false;
+    if (vec[i] <= vec[i + 1]) decreasing = false;
+  }
+  
   return increasing || decreasing;
 }
 
@@ -45,24 +51,12 @@ bool adiacentsDifferByThreeAtMost(const std::vector<int>& vec) {
   return true;
 }
 
-bool adiacentDifferByThreeAtMostWithTolerance(const std::vector<int>& vec) {
-  for(size_t i = 0; i < static_cast<int>(vec.size()); ++i) {
-    std::vector<int> temp(vec);
-    temp.erase(temp.begin() + i);
-
-    if(adiacentsDifferByThreeAtMost(temp)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-bool isMonotonicWithTolerance(const std::vector<int>& vec) {
+bool isSafe(const std::vector<int>& vec) {
+  if (vec.size() <= 2) return true;
   for(size_t i = 0; i < vec.size(); ++i) {
     std::vector<int> temp(vec);
     temp.erase(temp.begin() + i);
-
-    if(isMonotonic(temp)) {
+    if (adiacentsDifferByThreeAtMost(temp) && isMonotonic(temp)) {
       return true;
     }
   }
@@ -75,7 +69,7 @@ int main() {
   std::ofstream outputFile("unsafe.txt");
 
   for(size_t i = 0; i < matrix.size(); ++i) {
-    if(isMonotonicWithTolerance(matrix[i]) && adiacentDifferByThreeAtMostWithTolerance(matrix[i])) {
+    if(isSafe(matrix[i])) {
       ++safeReports;
     } else {
       for (const auto& j : matrix[i]) {
